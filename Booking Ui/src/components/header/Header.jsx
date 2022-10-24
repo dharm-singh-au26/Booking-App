61e21dcdc6eaf02fe130dae53315ac056ad1e032
Dharm Singh <dharmjat45@gmail.com>
2022-10-24 22:47:08 +0530

    booking date added i header

--- Booking Ui/src/components/header/Header.jsx
+++ Booking Ui/src/components/header/Header.jsx
@@ -5,9 +5,24 @@ import {
   faPlane,
   faTaxi,
   faCar,
+  faCalendarDays,
+  faPerson,
 } from "@fortawesome/free-solid-svg-icons";
+import { DateRange } from "react-date-range";
+import { format } from "date-fns";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useState } from "react";
 
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
   return (
     <div className="header">
       <div className="headerContainer">
@@ -34,12 +49,49 @@ const Header = () => {
           </div>
         </div>
         <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
-        <p className="headerDesc">Get rewarded for your travels – unlock instant savings of 10% or
-              more with a free DharmBooking account</p>
-              <button className="headerBtn">Sign in / Register</button>
-              <div className="headerSearch">
-                <div className="headerSearchItem"></div>
-              </div>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free DharmBooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going"
+              className="hederSearchInput"
+            />
+          </div>
+
+          {/* Calendar setup */}
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span onClick={()=> setOpenDate(!openDate)} className="headerSearchText">{`${format(
+              date[0].startDate,
+              "dd/MM/yyyy"
+            )} to ${format(date[0].endDate, "dd/MM/yyyy")} `}</span>
+            {/* condition check  */}
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            )}
+          </div>
+          {/* Person Info  */}
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 Adults 2 Children 1 Room</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
