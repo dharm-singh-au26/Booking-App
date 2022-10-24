128447942d42f107090c94515865ee75efa387db
Dharm Singh <dharmjat45@gmail.com>
2022-10-24 18:07:52 +0530

    Navbar created

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import Navbar from "../../components/navbar/Navbar"
+import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div><Navbar/></div>
   )
 }
 
