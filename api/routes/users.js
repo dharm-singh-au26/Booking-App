6be67f37272bd640094f06f265e89684ef9a958f
Dharm Singh <dharmjat45@gmail.com>
2022-10-24 14:05:48 +0530

    users route created

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,22 @@
 import express from "express";
+import { updateUser,deleteUser,getUser,getAllUsers } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkAuthentication",verifyToken,(req,res,next) =>{
+res.send("Hello user, you are logged in ")
+})
+//UPDATE
+router.put("/:id", updateUser);
+// //DELETE
+router.delete("/:id", deleteUser);
+
+// // Get
+router.get("/:id", getUser);
+// // GET ALL
+
+router.get("/", getAllUsers);
 
 
 export default router
