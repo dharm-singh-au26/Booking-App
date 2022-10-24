dd850940d922a47655d706c1e083f65d1573191e
Dharm Singh <dharmjat45@gmail.com>
2022-10-24 19:26:44 +0530

    Header started

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
+import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import "./Home.css"
 
 function Home() {
   return (
-    <div><Navbar/></div>
+    <div>
+
+    <Navbar/>
+    <Header/>
+    </div>
+    
+    
   )
 }
 
