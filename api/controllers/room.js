31c5d5a8165b4f712635eb972af4c6885bd0d794
Dharm Singh <dharmjat45@gmail.com>
2022-10-24 16:22:48 +0530

    updated room

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -18,3 +18,44 @@ export const createRoom  =  async (req,res,next)=>{
         next(error)
      }
 }
+
+export const updateRoom = async (req, res, next) => {
+    try {
+      const updatedRoom = await Room.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedRoom);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  export const deleteHotel = async (req, res, next) => {
+    try {
+      await Hotel.findByIdAndDelete(req.params.id);
+      res.status(200).json("Hotel Deleted");
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  export const getHotel = async (req, res, next) => {
+    try {
+      const hotel = await Hotel.findById(req.params.id);
+      res.status(200).json(hotel);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  export const getAllHotels = async (req, res, next) => {
+    try {
+      const hotels = await Hotel.find();
+      res.status(200).json(hotels);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
