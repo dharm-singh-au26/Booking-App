9ac2320a0deecdb7b2aff2f767a3fe70145aeaaa
Dharm Singh <dharmjat45@gmail.com>
2022-10-26 16:18:35 +0530

    propertyList done

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyList/PropertyList"
 import "./Home.css"
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
     <div className="homeContainer">
     <Featured/>
     <h1 className="homeTitles">Browse by property type</h1>
+    <PropertyList/>
 
     </div>
    
