d97d2e702292e48d066458a953df9ab74dae841f
Dharm Singh <dharmjat45@gmail.com>
2022-10-26 17:21:00 +0530

    mail List completed

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured"
 import { FeaturedProperties } from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
+import { MailList } from "../../components/mailList/MailList"
 import Navbar from "../../components/navbar/Navbar"
 import PropertyList from "../../components/propertyList/PropertyList"
 import "./Home.css"
@@ -17,6 +18,7 @@ function Home() {
     <PropertyList/>
     <h1 className="homeTitles">Homes Guest love </h1>
     <FeaturedProperties/>
+    <MailList/>
 
     </div>
    
