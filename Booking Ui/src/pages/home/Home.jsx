14c343d00da6325df8786928b5cfcf8761e19d43
Dharm Singh <dharmjat45@gmail.com>
2022-10-26 16:02:31 +0530

    Featured component start

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import "./Home.css"
@@ -8,6 +9,11 @@ function Home() {
 
     <Navbar/>
     <Header/>
+    <div className="homeContainer">
+    <Featured/>
+
+    </div>
+   
     </div>
     
     
