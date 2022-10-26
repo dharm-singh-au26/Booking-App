9bf763e4de07c931ab34bd9e9f0645d2b2d270c2
Dharm Singh <dharmjat45@gmail.com>
2022-10-26 16:31:05 +0530

    FeaturedProperties completed

--- Booking Ui/src/pages/home/Home.jsx
+++ Booking Ui/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured"
+import { FeaturedProperties } from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import PropertyList from "../../components/propertyList/PropertyList"
@@ -14,6 +15,8 @@ function Home() {
     <Featured/>
     <h1 className="homeTitles">Browse by property type</h1>
     <PropertyList/>
+    <h1 className="homeTitles">Homes Guest love </h1>
+    <FeaturedProperties/>
 
     </div>
    
