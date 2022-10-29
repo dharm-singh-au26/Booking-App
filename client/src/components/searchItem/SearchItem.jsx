d8ac199bacc9727c855b07b4a3ec41e0dd5ae4ca
Dharm Singh <dharmjat45@gmail.com>
2022-10-29 16:46:47 +0530

    context api

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -1,14 +1,10 @@
 import "./searchItem.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const SearchItem = ({item}) => {
+const SearchItem = ({ item }) => {
   return (
     <div className="searchItem">
-      <img
-        src={item.photos[0]}
-        alt=""
-        className="siImg"
-      />
+      <img src={item.photos[0]} alt="" className="siImg" />
       <div className="siDesc">
         <h1 className="siTitle">{item.name}</h1>
         <span className="siDistance">{item.distance} from center</span>
@@ -16,25 +12,24 @@ const SearchItem = ({item}) => {
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
         </span>
-        <span className="siFeatures">
-         {item.desc}
-        </span>
+        <span className="siFeatures">{item.desc}</span>
         <span className="siCancelOp">Free cancellation </span>
         <span className="siCancelOpSubtitle">
           You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className="siDetails">
-       { item.rating && <div className="siRating">
-          <span>Excellent</span>
-          <button>{item.rating}</button>
-        </div>}
+        {item.rating && (
+          <div className="siRating">
+            <span>Excellent</span>
+            <button>{item.rating}</button>
+          </div>
+        )}
         <div className="siDetailTexts">
           <span className="siPrice">${item.cheapestPrice}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <Link to = {`/hotels/${item._id}`}> 
-
-          <button className="siCheckButton">See availability</button>
+          <Link to={`/hotels/${item._id}`}>
+            <button className="siCheckButton">See availability</button>
           </Link>
         </div>
       </div>
